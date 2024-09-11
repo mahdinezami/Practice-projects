e6aff3d8bd8b6a2ba4d6eebbbcac4ff9f46d8285
mahdinezami <158995834+mahdinezami@users.noreply.github.com>
2024-09-12 01:11:52 +0330

    Create CharacterAndAsciiValue.c

---
+++ C Project/Mix01/Part1/CharacterAndAsciiValue.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+int main ()
+{
+    char my_charecter = 'c';
+    int my_ascii = my_charecter;
+    printf("the value is :%c\n", my_charecter);
+    printf("your ascii value is%d" , my_ascii);
+    return 0 ;
+}

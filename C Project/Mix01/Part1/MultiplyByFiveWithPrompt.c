161144d8c49e796fc578775b15593263ba87d929
mahdinezami <158995834+mahdinezami@users.noreply.github.com>
2024-09-12 01:13:32 +0330

    Create MultiplyByFiveWithPrompt.c

---
+++ C Project/Mix01/Part1/MultiplyByFiveWithPrompt.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+int main ()
+{
+    int a ;
+    puts("please enter a number:\n");
+    scanf("%d", &a);
+    int b  = a * 5 ;
+    printf("The value of your number multiplied by5= %d", b );
+    return 0 ;
+}
